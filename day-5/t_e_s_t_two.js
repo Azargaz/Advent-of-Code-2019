6ea08cc7e6473b10fc94cf511a9e72dab7e8dfcb
Azargaz <huba310@gmail.com>
2019-12-06 00:15:00 +0100

    Solution to day 5 part II.

--- day-5/t_e_s_t_two.js
+++ day-5/t_e_s_t_two.js
@@ -8,6 +8,7 @@ const t_e_s_t = (input, program) => {
     let skip = 2;
     let par1 = 0;
     let par2 = 0;
+    let par3 = 0;
 
     while(i < inputArray.length) {
         let intcode = inputArray[i].toString().split('');
@@ -18,10 +19,11 @@ const t_e_s_t = (input, program) => {
         skip = 2;
         par1 = 0;
         par2 = 0;
-        
+        par3 = 0;
+
         par1 = (intcode.length < 3 || intcode[2] === '0') ? inputArray[inputArray[i+1]] : inputArray[i+1];
         
-        if(intcode[0] === '1' || intcode[0] === '2') {
+        if(['1', '2', '5', '6', '7', '8'].includes(intcode[0])) {
             skip = 4;
             par2 = (intcode.length < 4 || intcode[3] === '0') ? inputArray[inputArray[i+2]] : inputArray[i+2];
             par3 = inputArray[i+3];
@@ -35,6 +37,32 @@ const t_e_s_t = (input, program) => {
             inputArray[inputArray[i+1]] = input;
         } else if(intcode[0] === '4') {
             output = par1;
+        } else if(intcode[0] === '5') {
+            if(par1 !== 0) {
+                i = par2;
+                continue;
+            } else {
+                skip = 3;
+            }     
+        } else if(intcode[0] === '6') {
+            if(par1 === 0) {
+                i = par2;
+                continue;
+            } else {
+                skip = 3;
+            }
+        } else if(intcode[0] === '7') {
+            if(par1 < par2) {
+                inputArray[par3] = 1;
+            } else {
+                inputArray[par3] = 0;
+            }
+        } else if(intcode[0] === '8') {
+            if(par1 === par2) {
+                inputArray[par3] = 1;
+            } else {
+                inputArray[par3] = 0;
+            }
         } else if(intcode.length === 2 && intcode[0] === '9' && intcode[1] === '9')
             break;
 
--- day-5/test.js
+++ day-5/test.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
 const t_e_s_t = require('./t_e_s_t');
+const t_e_s_t_two = require('./t_e_s_t_two');
 const expect = require('chai').expect;
 const fs = require('fs');
 
@@ -35,6 +36,113 @@ describe('Day 5 - Part I TEST solution', () => {
 	it('should return valid solution', () => {
 		const output = t_e_s_t(1, input);
 		console.log(`\tSolution to part I: ${output}`);
+		expect(output).to.be.a('number');
+	});
+});
+
+// Day 5 - part II tests
+describe('Day 5 - Part II TEST', () => {
+	let program = '3,9,8,9,10,9,4,9,99,-1,8';
+	it('should return 0 when input is lower than 8', () => {
+		assert.strictEqual(t_e_s_t_two(6, program), 0);
+	});
+	it('should return 0 when input is greater than 8', () => {
+		assert.strictEqual(t_e_s_t_two(10, program), 0);
+	});
+	it('should return 1 when input is 8', () => {
+		assert.strictEqual(t_e_s_t_two(8, program), 1);
+	});
+});
+
+describe('Day 5 - Part II TEST', () => {
+	let program = '3,9,7,9,10,9,4,9,99,-1,8';
+	it('should return 1 when input is lower than 8', () => {
+		assert.strictEqual(t_e_s_t_two(3, program), 1);
+	});
+	it('should return 0 when input is greater than 8', () => {
+		assert.strictEqual(t_e_s_t_two(99, program), 0);
+	});
+	it('should return 0 when input is 8', () => {
+		assert.strictEqual(t_e_s_t_two(8, program), 0);
+	});
+});
+
+describe('Day 5 - Part II TEST', () => {
+	let program = '3,3,1108,-1,8,3,4,3,99';
+	it('should return 0 when input is lower than 8', () => {
+		assert.strictEqual(t_e_s_t_two(1, program), 0);
+	});
+	it('should return 0 when input is greater than 8', () => {
+		assert.strictEqual(t_e_s_t_two(11, program), 0);
+	});
+	it('should return 1 when input is 8', () => {
+		assert.strictEqual(t_e_s_t_two(8, program), 1);
+	});
+});
+
+describe('Day 5 - Part II TEST', () => {
+	let program = '3,3,1107,-1,8,3,4,3,99';
+	it('should return 1 when input is lower than 8', () => {
+		assert.strictEqual(t_e_s_t_two(7, program), 1);
+	});
+	it('should return 0 when input is greater than 8', () => {
+		assert.strictEqual(t_e_s_t_two(9, program), 0);
+	});
+	it('should return 0 when input is 8', () => {
+		assert.strictEqual(t_e_s_t_two(8, program), 0);
+	});
+});
+
+describe('Day 5 - Part II TEST', () => {
+	let program = '3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9';
+	it('should return 0 when input is 0', () => {
+		assert.strictEqual(t_e_s_t_two(0, program), 0);
+	});
+	it('should return 1 when input is not 0', () => {
+		assert.strictEqual(t_e_s_t_two(10, program), 1);
+	});
+});
+
+describe('Day 5 - Part II TEST', () => {
+	let program = '3,3,1105,-1,9,1101,0,0,12,4,12,99,1';
+	it('should return 0 when input is 0', () => {
+		assert.strictEqual(t_e_s_t_two(0, program), 0);
+	});
+	it('should return 1 when input is not 0', () => {
+		assert.strictEqual(t_e_s_t_two(10, program), 1);
+	});
+});
+
+describe('Day 5 - Part II TEST', () => {
+	let program = '3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99';
+	it('should return 999 when input is lower than 8', () => {
+		assert.strictEqual(t_e_s_t_two(7, program), 999);
+	});
+	it('should return 1000 when input is equal to 8', () => {
+		assert.strictEqual(t_e_s_t_two(8, program), 1000);
+	});
+	it('should 1001 when input is greater than 8', () => {
+		assert.strictEqual(t_e_s_t_two(9, program), 1001);
+	});
+});
+
+describe('Day 5 - Part II TEST solution', () => {
+	let input;
+	it('should return not undefined', (done) => {
+		fs.readFile('./day-5/input.txt', 'utf8', (err, data) => {
+			if (err) {
+				console.error(err);
+				return;
+			}
+			input = data;
+			expect(input).to.be.not.undefined;
+			done();
+		});
+	});
+
+	it('should return valid solution', () => {
+		const output = t_e_s_t_two(5, input);
+		console.log(`\tSolution to part II: ${output}`);
 		expect(output).to.be.a('number');
 	});
 });
